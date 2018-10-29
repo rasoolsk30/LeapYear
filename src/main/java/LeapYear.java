49567a7a03ff7b8b2388b059d1d0b93654ab125f
rasool <rasools27@gmail.com>
2018-10-30 05:10:05 +0530

    push the project

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,26 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int year = 1900;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                // year is divisible by 400, hence the year is a leap year
+                leap = year % 400 == 0;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
---
+++ src/test/java/LeapYear.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+public class LeapYear {
+    
+    public LeapYear() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
